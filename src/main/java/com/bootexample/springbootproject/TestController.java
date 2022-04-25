6cb7497c4a6a393ab845a116ad9b637dd722849a
mansimalik456 <mansi.malik@oodles.io>
2022-04-25 19:03:38 +0530

    Second Maven Commit

---
+++ src/main/java/com/bootexample/springbootproject/TestController.java
@@ -0,0 +1,18 @@
+package com.bootexample.springbootproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class TestController {
+	
+	
+	@RequestMapping("/test")
+	@ResponseBody
+	public String firstHandler() {
+		return "Just for testing !! ";
+	}
+
+}
